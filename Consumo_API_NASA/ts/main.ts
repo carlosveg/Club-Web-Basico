9ee4cd7d3b7c25214aea59ce4bdc0507661e4a05
Carlos Raymundo Vega Gloria <carlosraymundovegagloria@gmail.com>
2021-04-06 14:42:57 -0500

    Consumiendo API de la NASA e inyectando el codigo en HTML

---
+++ Consumo_API_NASA/ts/main.ts
@@ -0,0 +1,27 @@
+const container = document.getElementById("api_info");
+
+const getDataAPI = async (): Promise<void> => {
+  const key: string = "XCe2rRk7qiPEGSrNxioNudgUuoyBNJy42FViZ8uc";
+  const res: Response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`);
+  const data = await res.json();
+
+  const result: string = generarTarjeta(data);
+
+  container.innerHTML = result;
+};
+
+const generarTarjeta = ({copyright, date, explanation, title, url}): string => {
+  return `
+    <section class="card">
+        <img src="${url}" alt="${title}">
+        <section class="data">
+          <h3>${title}</h3>
+          <h4>Author (copyright): ${copyright}</h4>
+          <h4>Date: ${date}</h4>
+          <p>${explanation}</p>
+      </section>
+    </section>
+    `;
+};
+
+getDataAPI();
